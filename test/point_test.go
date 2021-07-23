dfef8fcd5453a51d101a912d10c071e5b1f1c71e
hxd <hxd>
2021-07-23 14:39:17 +0800

    main

--- test/point_test.go
+++ test/point_test.go
@@ -1,9 +1,32 @@
 package test
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestPoint(t *testing.T) {
+	//指针调用
+	//定义一个数字
+	i := 10
+	fmt.Println("initial: ", i)
 
+	byValue(i)
+	fmt.Println("byval:", i)
+	// 地址传递给函数byValue
+	byPointer(&i)
+	fmt.Println("byptr:", i)
+	// 打印出 i 的地址
+	fmt.Println("pointer:", i)
+
+}
+
+func byPointer(iptr *int) {
+	//表示指针指向的数值
+	*iptr = 0
+
+}
+
+func byValue(ival int) {
+	ival = 0
 }
