79a390537e4bbcd3b5f7a5f37bf936cf1f16f0bc
hxd <hxd>
2021-07-23 12:02:35 +0800

    main

---
+++ test/point_test.go
@@ -0,0 +1,9 @@
+package test
+
+import (
+	"testing"
+)
+
+func TestPoint(t *testing.T) {
+
+}
--- test/recursion_test.go
+++ test/recursion_test.go
@@ -1,9 +1,17 @@
 package test
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestRecusion(t *testing.T) {
+	fmt.Println(sums(10))
+}
 
+func sums(num int) int {
+	if num == 1 {
+		return num
+	}
+	return sums(num-1) + num
 }
