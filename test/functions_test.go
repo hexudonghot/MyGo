306cf54d1a73c1acaadb87788bffdb1b92d31e1e
hxd <hxd>
2021-07-23 11:39:29 +0800

    main

---
+++ test/functions_test.go
@@ -0,0 +1,50 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+)
+
+/**
+函数的定义
+多返回值函数
+可变参数函数
+*/
+func plus2(a int, b int) int {
+	return a + b
+}
+
+// a+b+c
+func plus3(a, b, c int) int {
+	return a + b + c
+}
+
+// 四则计算：计算两个值的加减乘除结果，返回多个值
+func calABCD(a, b int) (int, int, int, int) {
+	return a + b, a - b, a * b, a / b
+}
+
+// 可变参数函数：合计参数值
+func sum(nums ...int) int {
+	fmt.Print(nums, " ")
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	return total
+}
+
+func TestFc(t *testing.T) {
+	// 两个数相加
+	result := plus2(10, 20)
+	fmt.Println("10+20=", result)
+	// 三个数相加
+	result = plus3(10, 20, 30)
+	fmt.Println("10+20+30=", result)
+	// 两个数计算加减乘除
+	a, b, c, d := calABCD(200, 100)
+	fmt.Println("ab四则计算=", a, b, c, d)
+	// 可变参数函数计算合集
+	result = sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+	fmt.Println("1-10合计为=", result)
+}
