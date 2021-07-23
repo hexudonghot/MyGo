0a2efb58c533345845abb3511a4ab9d68c9c868f
hxd <hxd>
2021-07-23 10:35:23 +0800

    main

--- main.go
+++ main.go
@@ -1,8 +1,27 @@
-package main
+package test
 
 import "fmt"
 
 func main() {
-	fmt.Println("hello world ")
+	/**
+	  array------start
+	*/
+	var a [5]int
+	//fmt.Println(a)
+	a[4] = 6
+	//fmt.Println(a)
+
+	//fmt.Println(len(a))
+	b := [5]int{1, 2, 3, 4, 5}
+	//fmt.Println(b)
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(b[i])
+	}
+	/**
+	  array------end
+	*/
+	s := make([]string, 3)
+	fmt.Println(s)
 
 }
---
+++ test/slince_test.go
@@ -0,0 +1,11 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLog(t *testing.T) {
+	s := make([]string, 3)
+	fmt.Println(s)
+}
